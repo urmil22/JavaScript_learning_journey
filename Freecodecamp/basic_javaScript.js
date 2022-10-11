7061f58ded8e992a53152600afd31a5565e0847e
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-10-11 23:35:35 +0100

    first commit

---
+++ Freecodecamp/basic_javaScript.js
@@ -0,0 +1,39 @@
+// let is a better alternative to var if we don't 
+// want to update the variable, let was introduced to 
+// in ES6
+
+// var is easily overriden
+
+
+// naming variables that we don't want to ressign 
+// using const
+
+
+// declaring a constans with uppercase is btter somehow
+
+
+
+// numbers
+
+// incremmentaio of variabl using i++
+//decrementing using i--
+
+
+// remainder operator 
+// % example : 
+// 7 % 2 = 1
+
+// quoitient
+// math.floor(5/2) = 2 
+
+
+// everything to the right of the equals sign is
+// evaluated first
+// myVar = myVar + 1
+
+
+// compound assignment 
+// a += 1 means a = a + 1
+// a -= 1 means a = a - 1
+// a *= 1 means a = a * 1
+// a /= 1 means a = a / 1
