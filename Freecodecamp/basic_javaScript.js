5a9b82383be1ec0e224a99fc3d7e77a9e06517a2
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-10-15 23:13:02 +0100

    basic javascript

--- Freecodecamp/basic_javaScript.js
+++ Freecodecamp/basic_javaScript.js
@@ -37,3 +37,57 @@
 // a -= 1 means a = a - 1
 // a *= 1 means a = a * 1
 // a /= 1 means a = a / 1
+
+
+// Use Bracket Notation to Find the First Character in a String
+
+// const firstName = "Charles";
+// const firstLetter = fistName[0];
+
+
+// Understanding String Immutability
+// string values in JS are immutable; which means
+// they can not be altered once created
+
+// let myStr = "Bob";
+// myStr[0] = "J";
+// let myStr = "Bob";
+// myStr = "Job";
+
+
+// Use Bracket Notation to Find the Nth Character in a String
+
+// const firstName = "Ada";
+// const secondLetterOfFirstName = firstName[1];
+
+// Use Bracket Notation to Find the Last Character in a String
+// const firstName ="Ada";
+// lastLetter = firstName[firstName.length - 1]
+
+
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+// const firstName = "Augusta";
+// thisrdToLastLetter = firstName[firstName.length - 3]
+
+//Store multiple values in one variabl using javaScript Arrays
+// const sandwich = ["peanut butter","jelly","bread"]
+
+// Nest one Array within Another Array
+// const myArray = [["Bulls", 25],["Chickens", 12]]
+
+// Access Array Data with Indexes
+// we can access data with indexes
+
+// const array = [50, 60 ,70];
+// console.log(array[0]);
+// const data = array[1];
+
+
+// Access Array Data with Indexes
+// Unlike strings, arrays are mutable and can be 
+// changed freely.
+// const ourArray = [50, 40, 90];
+// ourArray[0] = 15
+
+
+// Access Multi-Dimensional Arrays With Indexes
